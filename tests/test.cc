1ae4f646a51d22201b4069d7e9f03325fa74a0af
root <root@localhost.localdomain>
2021-09-28 16:39:19 +0800

    注释代码

--- tests/test.cc
+++ tests/test.cc
@@ -2,6 +2,7 @@
 #include "../kyubi/log.h"
 #include "../kyubi/util.h"
 int main(int argc,char** argv){
+    #if 0
     std::cout<<"hello world" << std::endl;
     kyubi::Logger::ptr logger(new kyubi::Logger());
     logger->addAppender(kyubi::LogAppender::ptr(new kyubi::StdoutAppender));
@@ -13,9 +14,9 @@ int main(int argc,char** argv){
     file_appender->setFormatter(fmt);
     logger->addAppender(file_appender);
     std::cout << "hello kyubi"<<std::endl;
-    KYUBI_LOG_INFO(logger) << "test macro";
-    KYUBI_LOG_ERROR(logger) << "test macro error";
-
+    //KYUBI_LOG_INFO(logger) << "test macro";
+    //KYUBI_LOG_ERROR(logger) << "test macro error";
+    #endif
     auto l = kyubi::LogMgr::GetInstance()->getLogger("xx");
     KYUBI_LOG_INFO(l) <<"xxx";
     return 0;
